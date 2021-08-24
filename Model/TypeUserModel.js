a710f438231c5360f4c2c3873426930bb69db1fd
Eduardo Weber <edu.weber07@gmail.com>
2021-08-24 16:06:20 -0300

    TypeUser feito

---
+++ Model/TypeUserModel.js
@@ -0,0 +1,5 @@
+export class TypeUserModel{
+    id;
+    name;
+    description;
+}
---
+++ Repository/TypeUserRepository.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export class TypeUserRepository{
+    api_connect="http://192.168.0.141/api/";
+    api;
+
+    constructor(){
+        this.api = axios.create({baseURL:this.api_connect})
+    }
+
+    create(model){
+        this.api.post("TypeUser", model).then((x) => console.log(x.status))
+    }
+
+    async Read(){
+        return (await(this.api.get("TypeUser").then())).data
+    }
+
+    async ReadById(id){
+        return (await(this.api.get(`TypeUser/${id}`).then())).data
+    }
+}
