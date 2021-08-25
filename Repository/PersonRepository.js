3a808e96f0c0947c3276a6fe39462d8f237629c2
Eduardo Weber <edu.weber07@gmail.com>
2021-08-25 10:07:22 -0300

    Generics feito

--- Repository/PersonRepository.js
+++ Repository/PersonRepository.js
@@ -1,28 +1,7 @@
-import axios from "axios";
+import { BaseRepository } from "./BaseRepository.js";
 
-export class PersonRepository{
-    api_connect="http://192.168.0.141/api/";
-    api;
-
+export class PersonRepository extends BaseRepository{
     constructor(){
-        this.api = axios.create({baseURL:this.api_connect})
-    }
-
-    create(model){
-        this.api.post("Person", model).then((x) => console.log(x.status))
-    }
-
-    async Read(){
-        return (await(this.api.get("Person").then())).data
-    }
-
-    async ReadById(id){
-        return (await(this.api.get(`Person/${id}`).then())).data
-    }
-    uptade(model){
-        this.api.put(`Person/${model.id}`,model).then(x => console.log(x.status));
-    }
-    delete(id){
-        this.api.delete(`Person/${id}`).then(x => console.log(x.status));
+        super("Person");
     }
 }
